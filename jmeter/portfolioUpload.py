570f2a2bce596cd3b3a3f62d35466994bc446168
yckong <yckong@palo-it.com>
2018-01-22 10:36:14 +0800

    OW-761 removed hardcoded portfolio id

--- jmeter/portfolioUpload.py
+++ jmeter/portfolioUpload.py
@@ -1,24 +1,61 @@
+#################################################
+#File creation and modification
+#Generate UTI
+#Generate Portfolio IDs
+#Auto Configure Body Parameter for Jmeter
+#################################################
+
 import openpyxl
 import sys
 import os
 import glob
 import time
+from itertools import cycle
+import ConfigParser
 
 #recording total time for the execution of python script
-f=open('time.log','a')
+timelog=open('time.log','a')
 start_time=time.time()
 
+#Collect argv from config file
+config = ConfigParser.RawConfigParser()
+config.read('test.cfg')
+argv1=config.getint('config','threads')
+argv2=config.getint('config','rampup')
+argv3=config.getint('config','loop')
+argv4=config.getint('config','transactions')
+argv5=config.get('config','portfolioIDs')
+argv6=config.get('config','filename')
+
+
+#define arguments
+Plist=argv5.split(",")
 count=0
-tcount=str(sys.argv[1])
-thread=int(sys.argv[1])
-rampup=str(sys.argv[2])
-loop=str(sys.argv[3])
-rowcount=int(sys.argv[4])
+tcount=str(argv1)
+thread=int(argv1)
+rampup=str(argv2)
+loop=str(argv3)
+rowcount=int(argv4)
+
+#Configure BodyData for portfolioids
+PortfolioIDs_BD="{\"ids\":["
+for items in Plist:
+	PortfolioIDs_BD=PortfolioIDs_BD+'"'+items+'"'+","
+BPortfolioIDs_BD=PortfolioIDs_BD.rstrip(',')
+PortfolioIDs_BD=PortfolioIDs_BD+"]}"
+
+#write portfolio IDs
+portfolio_txt=open("portfolioids.txt","w")
+portfolio_txt.write(PortfolioIDs_BD)
+portfolio_txt.close()
+
+#portfolio ID list
+pool = cycle(Plist)
 
 #While loop for file creation
 while count<thread:
     count=count+1
-    f1=openpyxl.load_workbook('demo.xlsx')
+    f1=openpyxl.load_workbook(argv6)
     s1=f1.get_sheet_by_name('IRS-Cleared')
     s2=f1.get_sheet_by_name('FRA-Cleared')
     s3=f1.get_sheet_by_name('OIS-Cleared')
@@ -26,15 +63,36 @@ while count<thread:
     s5=f1.get_sheet_by_name('FXSwap-Bilateral')
     s6=f1.get_sheet_by_name('ZCS-Cleared')
    
-
-   #While loop for creation of new UTI value depend of row count 
+   #Loop for file modification(UTI, portfolio IDs)
     row = 1
     transaction=0
     while row<=rowcount:
         row=row+1
         transaction=transaction+1
+
+        ID=pool.next()
+        
+        #UTI cell number
         rowD='D'+str(row)
 
+        #Portfolio cell number
+        tab1='AX'+str(row)
+        tab2='AK'+str(row)
+        tab3='AY'+str(row)
+        tab4='AQ'+str(row)
+        tab5='AI'+str(row)
+        tab6='AY'+str(row)
+
+        #Excel file modification
+        #Portfolio Modification
+        s1[tab1]=str(ID)
+        s2[tab2]=str(ID)
+        s3[tab3]=str(ID)
+        s4[tab4]=str(ID)
+        s5[tab5]=str(ID)
+        s6[tab6]=str(ID)
+
+        #UTI Number Generation
         val1=str(count)+'1'+str(transaction)
         val2=str(count)+'2'+str(transaction)
         val3=str(count)+'3'+str(transaction)
@@ -42,7 +100,7 @@ while count<thread:
         val5=str(count)+'5'+str(transaction)
         val6=str(count)+'6'+str(transaction)
 
-
+        #UTI Modification
         s1[rowD]=int(val1)
         s2[rowD]=int(val2)
         s3[rowD]=int(val3)
@@ -53,13 +111,14 @@ while count<thread:
     f1.save('load%d.xlsx' % count)
 
 #Execution of load test through Jmeter
-os.system("Jmeter -n -t portfolioUpload.jmx -l result.csv -J user.count=%s -J user.rampup=%s -J user.loop=%s" %(tcount,rampup,loop))
+#os.system("Jmeter -n -t UploadValuation.jmx -l result.csv -J user.count=%s -J user.rampup=%s -J user.loop=%s" %(tcount,rampup,loop))
+
 
 end_time=time.time()
 total=end_time-start_time
-f.write('Start Time : '+str(start_time)+'\t'+'End Time : '+str(end_time)+'\t'+'Total Time : '+str(total)+'sec'+'\n')
-f.close()
+timelog.write('Start Time : '+str(start_time)+'\t'+'End Time : '+str(end_time)+'\t'+'Total Time : '+str(total)+'sec'+'\n')
+timelog.close()
 
 #Deletion of files.
-for name in glob.glob('load*.xlsx'):
-	os.remove(name)
+#for name in glob.glob('load*.xlsx'):
+#	os.remove(name)
